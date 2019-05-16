5488795c2cbe2264ecb095f7c2b657c80e29d7f7
sowmyay <sowmyayellapragada@gmail.com>
2019-05-16 20:56:54 +0000

    Added model architecture

--- data/path_editor.py
+++ data/path_editor.py
@@ -1,15 +1,20 @@
 import pandas as pd
+import cv2 
 
 def read_data():
-    df = pd.read_csv('driving_log.csv', header=None, names=['Center image', 'Left image', 'Right image', 'Steering Angle', 'Throttle', 'Break', 'Speed'])
-    df['Center image'] = df['Center image'].str.split('/').str[6]
-    df['Name'] = df['Center image'].str.split('_').str[1:]
+    df = pd.read_csv('driving_log.csv', header=None, names=['Center', 'Left', 'Right', 'Steering', 'Throttle', 'Break', 'Speed'])
+    df['Center'] = 'data/IMG/' + df['Center'].str.split('/').str[6]
+    df['Name'] = df['Center'].str.split('_').str[1:]
     df['Name'] = df['Name'].apply('_'.join)
     # df['name'] = '_'.join(df['Center image'].str.split('_').str[1:])
-    df['Left image'] = df['Left image'].str.split('/').str[6]
-    df['Right image'] = df['Right image'].str.split('/').str[6]
-    df = df[['Center image', 'Left image', 'Right image', 'Name', 'Steering Angle', 'Throttle', 'Break', 'Speed']]
+    df['Left'] = 'data/IMG/' + df['Left'].str.split('/').str[6]
+    df['Right'] = 'data/IMG/' + df['Right'].str.split('/').str[6]
+    df = df[['Center', 'Left', 'Right', 'Name', 'Steering', 'Throttle', 'Break', 'Speed']]
     return df
 
 df = read_data()
+df.to_csv('driving_log_1.csv')
 print(df[0:5])
+img = cv2.imread('IMG/center_2019_05_12_08_56_25_090.jpg')
+
+print(img.shape)
