63c2b6555465c73518767d135c85e88be81ff4ac
sowmyay <sowmyayellapragada@gmail.com>
2019-05-17 14:11:42 +0000

    Models generated

--- model.py
+++ model.py
@@ -5,11 +5,69 @@ from keras.models import Sequential
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
 from keras.layers import Lambda, Conv2D, MaxPooling2D, Dropout, Dense, Flatten
-from utils import INPUT_SHAPE, generator
 import argparse
 import os
+import cv2, os
+import matplotlib.image as mpimg
+from sklearn.utils import shuffle
 
 np.random.seed(0)
+IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS = 160, 320, 3
+INPUT_SHAPE = (IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)
+
+
+def preprocess(img):
+    """
+    Combine all preprocess functions into one
+    """
+    img = img[60:-25, :, :]
+    img = cv2.resize(img, (IMAGE_WIDTH, IMAGE_HEIGHT), cv2.INTER_AREA)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+    return img
+
+
+def choose_image(center, left, right, steering_angle, choice= np.random.choice(3), flip=False):
+    """
+    Randomly choose an image from the center, left or right, and adjust
+    the steering angle.
+    """
+
+    img, angle = cv2.imread(center), steering_angle
+    if choice == 0:
+        img, angle = cv2.imread(left), steering_angle + 0.2
+    elif choice == 1:
+        img, angle = cv2.imread(right), steering_angle - 0.2
+
+    if flip:
+        img = cv2.flip(img,1)
+        angle = -1*angle
+
+    return img, angle
+
+
+def generator(paths, angles, batch_size, is_training):
+    """
+    Generate training image give image paths and associated steering angles
+    """
+    images = np.empty([batch_size, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS])
+    steers = np.empty(batch_size)
+    while True:
+        i = 0
+        for index in np.random.permutation(paths.shape[0]):
+            center, left, right = paths[index]
+            steering_angle = angles[index]
+            # argumentation
+            if is_training and np.random.rand() < 0.6:
+                image, steering_angle = choose_image(center, left, right, steering_angle, flip=(np.random.rand()<0.5))
+            else:
+                image = cv2.imread(center)
+            # add the image and steering angle to the batch
+            images[i] = preprocess(image)
+            steers[i] = steering_angle
+            i += 1
+            if i == batch_size:
+                break
+        yield images, steers
 
 def load_data(args):
     """
@@ -85,7 +143,7 @@ def main():
     parser.add_argument('-d', help='data directory',        dest='data_dir',          type=str,   default='data')
     parser.add_argument('-t', help='test size fraction',    dest='test_size',         type=float, default=0.2)
     parser.add_argument('-k', help='drop out probability',  dest='keep_prob',         type=float, default=0.5)
-    parser.add_argument('-n', help='number of epochs',      dest='nb_epoch',          type=int,   default=10)
+    parser.add_argument('-n', help='number of epochs',      dest='nb_epoch',          type=int,   default=4)
     parser.add_argument('-s', help='samples per epoch',     dest='samples_per_epoch', type=int,   default=20000)
     parser.add_argument('-b', help='batch size',            dest='batch_size',        type=int,   default=40)
     parser.add_argument('-o', help='save best models only', dest='save_best_only',    type=s2b,   default='true')
